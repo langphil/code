2aa411e80193f66bc4447f3049fbd4a17387c9f1
Phil Lang <plangphoto@gmail.com>
2016-07-20 21:17:22 +0100

    changes host and port to work for heroku

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const port = '8080';
-const host = '0.0.0.0';
+const port = process.env.PORT || '8080';
+const host = process.env.PORT || '0.0.0.0';
 const express = require ('express');
 const app = express();
 
 //create variable look for index.js directory
 var rules = require('./app');
 //call variable - the 'boo' allows me to call a nested structure
-app.use("/boo", rules);
+app.use(rules);
 
 app.listen(port, host);
 
