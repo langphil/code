b6f1294b1c11c0fddb4c16168164c2c40fb9f554
Phil Lang <plangphoto@gmail.com>
2016-07-20 22:06:55 +0100

    added pick-one dependancy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 //create variable look for index.js directory
 var rules = require('./app');
-//call variable - the 'boo' allows me to call a nested structure
+
 app.use(rules);
 
 app.listen(port, host);
